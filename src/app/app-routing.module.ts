58dd76e5b99ac0da112e9792241eadd128ce611d
rahmanar007 <51256272+rahmanar007@users.noreply.github.com>
2019-06-01 11:42:22 +0530

    Add files via upload

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,10 @@ import { Routes, RouterModule } from '@angular/router';
 import {CustomerEntryComponent} from './customer-entry/customer-entry.component';
 
 const routes: Routes = [
-  { path: 'customer-entry', component: CustomerEntryComponent },
+  { path: 'user', component: CustomerEntryComponent },
 
   { path: '',
-  redirectTo: '/customer-entry',
+  redirectTo: '/user',
   pathMatch: 'full'
   }
 ];
